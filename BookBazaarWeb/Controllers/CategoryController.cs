be4465d3f164c0083f53d236e18b421d655b2117
Rares <raresmusea001@gmail.com>
2023-10-09 00:00:21 +0300

    Added styling for both navbar and footer, along with hover transitions and animations.

--- BookBazaarWeb/Controllers/CategoryController.cs
+++ BookBazaarWeb/Controllers/CategoryController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookBazaarWeb.DataContext;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookBazaarWeb.Controllers;
 
 [ApiController]
 public class CategoryController : Controller
 {
+    private readonly AppDataContext _context;
+
+    public CategoryController(AppDataContext context)
+    {
+        _context = context;
+    }
+
     [Route("{controller}")]
     public IActionResult Index()
     {
-        return View();
+        var categoryList = _context.Categories.ToList();
+        return View(categoryList);
     }
 }
