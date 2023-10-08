675ac497a9267be48deb9bcfd6a0f3b4ade6b6a6
Rares <raresmusea001@gmail.com>
2023-10-08 17:56:01 +0300

    Created the Categories table within the database by using EF Core migrations and seeded it. Started implementing the Category Controller.

---
+++ BookBazaarWeb/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookBazaarWeb.Controllers;
+
+[ApiController]
+public class CategoryController : Controller
+{
+    [Route("{controller}")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- BookBazaarWeb/DataContext/AppDataContext.cs
+++ BookBazaarWeb/DataContext/AppDataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookBazaarWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookBazaarWeb.DataContext;
 
@@ -6,5 +7,16 @@ public class AppDataContext : DbContext
 {
     public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
     {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Genre = "Adventure", Priority = 1 },
+            new Category { Id = 2, Genre = "Science-Fiction", Priority = 1 },
+            new Category { Id = 3, Genre = "Psychology", Priority = 1 }
+        );
     }
 }
