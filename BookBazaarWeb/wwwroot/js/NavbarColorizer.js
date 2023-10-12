d54919520a4f3bc3751069f9536ae9ace4a34fcb
Rares <raresmusea001@gmail.com>
2023-10-13 00:54:25 +0300

    Started working on the Create operation View for the book category entity. Implemented a custom navbar colorizer which colorizes the navbar links according to the current page on which the user navigates.

---
+++ BookBazaarWeb/wwwroot/js/NavbarColorizer.js
@@ -0,0 +1,26 @@
+﻿const currentPage = window.location.href;
+console.log(currentPage);
+const pagesText = ["Home", "Privacy", "Category"];
+const pagesDOM = document.querySelectorAll(".NavbarLink");
+const color = "#ED5B2D";
+
+const revalidateColors = () => {
+    for (let j = 0; j < pagesDOM.length; j++) {
+        if (pagesDOM[j].hasAttribute('style')) {
+            console.log(pagesDOM[j]);
+            pagesDOM.removeAttribute('style');
+        }
+    }
+}
+
+if (currentPage === "https://localhost:7279/" || currentPage === ("https://localhost:7229/Home")) {
+    revalidateColors();
+    pagesDOM[0].setAttribute('style', `color:${color} !important`);
+} else {
+    for (let i = 1; i < pagesText.length; i++) {
+        if (currentPage.includes(pagesText[i])) {
+            revalidateColors();
+            pagesDOM[i].setAttribute('style', `color:${color} !important`);
+        }
+    }
+}
