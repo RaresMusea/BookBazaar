cb30bed514f2ad4cd912412f8c5bd694a025b104
Rares <raresmusea001@gmail.com>
2023-10-22 23:26:17 +0300

    Implemented the functionality for the Book entity deletion, based on the Category entity functionality.

---
+++ BookBazaarWeb/wwwroot/js/ajax/DeleteBook.js
@@ -0,0 +1,71 @@
+const buttons = document.querySelectorAll('.DeleteButton');
+const urls = [];
+
+buttons.forEach(button => {
+    urls.push(button.dataset.url);
+})
+
+const displaySuccesDialog = (entityName) => {
+    Swal.fire({
+        icon: 'success',
+        html: `<h4>The ${entityName} was deleted successfully!.</h4>`,
+        showConfirmButton: true,
+        confirmButtonText: "OK",
+        confirmButtonColor: "#ED5B2D",
+    }).then(result => {
+        if (result.isConfirmed) {
+            location.reload();
+        }
+    })
+}
+
+const displayFailureDialog = (entityName) => {
+    Swal.fire({
+        icon: 'error',
+        html: `<h4>An error occurred while attempting to delete the ${entityName}.</h4>`,
+        showConfirmButton: true,
+        confirmButtonText: "Retry",
+        confirmButtonColor: "#545454",
+    }).then(result => {
+        if (result.isConfirmed) {
+            location.reload();
+        }
+    })
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', async (e) => {
+        fetch(urls[i])
+            .then(response => response.text())
+            .then(data => {
+                console.log(data)
+                Swal.fire({
+                    template: "#SwalDelModal",
+                    html: data,
+                    showCancelButton: true,
+                    showConfirmButton: true,
+                    confirmButtonText: "Delete",
+                    confirmButtonColor: "red",
+                    icon: 'question',
+                }).then(result => {
+                        if (result.isConfirmed) {
+                            console.log(document.querySelector(".ModalQuestion").dataset.payload);
+                            const payloadJson = JSON.parse(document.querySelector(".ModalQuestion").dataset.payload);
+                            $.ajax({
+                                url: '/Admin/Book/Delete',
+                                type: 'POST',
+                                data: payloadJson,
+                                success: () => {
+                                    displaySuccesDialog('book');
+                                },
+                                error: () => {
+                                    displayFailureDialog('book');
+                                }
+                            });
+                        }
+                    }
+                );
+            })
+            .catch(console.log);
+    });
+}
