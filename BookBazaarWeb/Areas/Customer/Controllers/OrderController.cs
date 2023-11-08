dfc2ba0791e387fc89f6ed6136cbcd2e24fa5adb
Rares <raresmusea001@gmail.com>
2023-11-08 02:26:56 +0200

    Separated the order details from users and admins/internals. Now, each customer has its own order section, where they can see their orders, along with their updated state. The feature uses JavaScript Fetch API to dynamically render the status for each order, which is dispatched. The request is returning the html of a partial view which is injected within a special wrapper in real time.

---
+++ BookBazaarWeb/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Claims;
+using BookBazaar.Data.Repo.Interfaces;
+using BookBazaar.Models.OrderModels;
+using BookBazaar.Models.VM;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookBazaarWeb.Areas.Customer.Controllers;
+
+[Area("Customer")]
+public class OrderController : Controller
+{
+    private readonly IWorkUnit _workUnit;
+
+    public OrderController(IWorkUnit workUnit)
+    {
+        _workUnit = workUnit;
+    }
+
+    public async Task<IActionResult> Index(int? id = null)
+    {
+        ClaimsIdentity claimsIdentity = (ClaimsIdentity)User.Identity!;
+        string loggedInUserId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+        IEnumerable<Order> orders = await _workUnit.OrderRepo.RetrieveAllAsync(o => o.UserId == loggedInUserId);
+        IList<OrderViewModel> viewModel = new List<OrderViewModel>();
+
+        foreach (var order in orders)
+        {
+            IEnumerable<OrderInfo> ordersInfo =
+                await _workUnit.OrderInfoRepo.RetrieveAllAsync(oi => oi.OrderId == order.Id);
+            viewModel.Add(new()
+            {
+                Order = order,
+                OrderInfos = ordersInfo
+            });
+        }
+
+        return View(viewModel);
+    }
+
+    public async Task<IActionResult> Details(int orderId)
+    {
+        Order order = await _workUnit.OrderRepo.GetAsync(o => o.Id == orderId, "User");
+
+        if (order is null)
+        {
+            return NotFound();
+        }
+
+        IEnumerable<OrderInfo> infos =
+            await _workUnit.OrderInfoRepo.RetrieveAllAsync(oi => oi.OrderId == orderId, "Book,InventoryItem");
+
+        OrderViewModel viewModel = new()
+        {
+            Order = order,
+            OrderInfos = infos,
+        };
+
+        return PartialView("_OrderDetails", viewModel);
+    }
+}
---
+++ BookBazaarWeb/wwwroot/js/ajax/Order/AppedOrderDetails.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const orderButtons = document.querySelectorAll('.OrderElement');
+    const target = document.querySelector('.OrderDetailsWindow');
+
+    console.log("MERGE!");
+    orderButtons.forEach(orderButton => {
+        orderButton.addEventListener('click', () => {
+            const url = orderButton.dataset.url;
+            fetch(url)
+                .then(response => response.text())
+                .then(data => {
+                    target.innerHTML = data;
+                })
+                .catch(console.log);
+
+        });
+    });
+});
