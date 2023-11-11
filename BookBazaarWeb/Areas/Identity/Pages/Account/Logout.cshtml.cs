6992331fe33e8c28a2951fe7e3da2365fb558fc6
Rares <raresmusea001@gmail.com>
2023-11-12 01:56:15 +0200

    Added a View component for dynamically managing the order basket count from session rather than managing it directly from the view itself.

--- BookBazaarWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ BookBazaarWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -23,7 +23,6 @@ namespace BookBazaarWeb.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            HttpContext.Session.Clear();
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
