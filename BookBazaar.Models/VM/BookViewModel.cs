d5f2ec767a34fcf31d30a2fe85792b698011aad6
Rares <raresmusea001@gmail.com>
2023-11-12 21:05:18 +0200

    Added a new dropdown menu that will display books, based on a selected category from the dropdown menu, on a new view, without showing the jumbotron-carousel.

--- BookBazaar.Models/VM/BookViewModel.cs
+++ BookBazaar.Models/VM/BookViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using BookBazaar.Models.BookModels;
+﻿using BookBazaar.Models.BookModels;
 using BookBazaar.Models.InventoryModels;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,5 +9,7 @@ public class BookViewModel
 {
     public Book? Book { get; set; }
     [ValidateNever] public IEnumerable<SelectListItem>? CategoriesList { get; set; }
-    [ValidateNever] public InventoryItem? InventoryItem { get; set; } 
+    [ValidateNever] public InventoryItem? InventoryItem { get; set; }
+
+    [ValidateNever] public string CategoryQuery { get; set; } = string.Empty;
 }
---
+++ BookBazaarWeb/Areas/Customer/Controllers/BookController.cs
@@ -0,0 +1,45 @@
+﻿using BookBazaar.Data.Repo.Interfaces;
+using BookBazaar.Models.BookModels;
+using BookBazaar.Models.VM;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookBazaarWeb.Areas.Customer.Controllers;
+
+[Area("Customer")]
+public class BookController : Controller
+{
+    private readonly IWorkUnit _workUnit;
+
+    public BookController(IWorkUnit workUnit)
+    {
+        _workUnit = workUnit;
+    }
+
+    public async Task<IActionResult> Index(string category = "All")
+    {
+        IEnumerable<Book> books = await _workUnit.BookRepo.RetrieveAllAsync(includedProperties: "Category");
+
+        if (category != "All")
+        {
+            books = FilterBooksBasedOnCategory(category, books);
+        }
+
+        List<BookViewModel> viewModels = new();
+        foreach (var book in books)
+        {
+            viewModels.Add(new()
+            {
+                Book = book,
+                InventoryItem = await _workUnit.InventoryRepo.GetAsync(inv => inv.BookId == book.Id),
+                CategoryQuery = category
+            });
+        }
+
+        return View(viewModels.OrderByDescending(o => o.InventoryItem!.QuantityInStock).ToList());
+    }
+
+    private IEnumerable<Book> FilterBooksBasedOnCategory(string category, IEnumerable<Book> books)
+    {
+        return books.Where(b => b.Category!.Genre == category);
+    }
+}
