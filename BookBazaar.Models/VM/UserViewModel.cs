6dbaa5096dcac09ba678142c45d8c15f5926df90
Rares <raresmusea001@gmail.com>
2023-11-12 22:38:22 +0200

    Added Roles to the Users management view.

---
+++ BookBazaar.Models/VM/UserViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BookBazaar.Models.VM;
+
+public class UserViewModel
+{
+    public AppUser.AppUser User { get; set; } = null!;
+    public string? Role { get; set; } = string.Empty;
+}
--- BookBazaarWeb/Areas/Admin/Controllers/UserController.cs
+++ BookBazaarWeb/Areas/Admin/Controllers/UserController.cs
@@ -1,8 +1,12 @@
-﻿using BookBazaar.Data.Repo.Interfaces;
+﻿using BookBazaar.Data.DataContext;
+using BookBazaar.Data.Repo.Interfaces;
 using BookBazaar.Misc.Roles;
 using BookBazaar.Models.AppUser;
+using BookBazaar.Models.VM;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookBazaarWeb.Areas.Admin.Controllers;
 
@@ -11,15 +15,35 @@ namespace BookBazaarWeb.Areas.Admin.Controllers;
 public class UserController : Controller
 {
     private readonly IWorkUnit _workUnit;
+    private readonly AppDataContext _context;
 
-    public UserController(IWorkUnit workUnit)
+    public UserController(IWorkUnit workUnit, AppDataContext context)
     {
         _workUnit = workUnit;
+        _context = context;
     }
 
     public async Task<IActionResult> Index()
     {
         IEnumerable<AppUser> users = await _workUnit.UserRepo.RetrieveAllAsync(includedProperties: "Company");
-        return View(users);
+
+        var userRoles = await _context.UserRoles.ToListAsync();
+        IList<IdentityRole> roles = await _context.Roles.ToListAsync();
+        IList<UserViewModel> viewModels = new List<UserViewModel>();
+
+        foreach (var user in users)
+        {
+            string roleIdentifier = userRoles.FirstOrDefault(r => r.UserId == user.Id)!.RoleId;
+            string? role = roles.FirstOrDefault(r => r.Id == roleIdentifier)!.Name;
+
+            viewModels.Add(
+                new UserViewModel
+                {
+                    User = user,
+                    Role = role,
+                });
+        }
+
+        return View(viewModels);
     }
 }
