488cbcec3d14be5f4853f36e44b493343b74ef72
Rares <raresmusea001@gmail.com>
2023-11-13 02:12:02 +0200

    Added the possibility of managing an account as an admin and to modify the role and permissions of a certain user.

--- BookBazaarWeb/Areas/Admin/Controllers/UserController.cs
+++ BookBazaarWeb/Areas/Admin/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using BookBazaar.Models.VM;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookBazaarWeb.Areas.Admin.Controllers;
@@ -16,11 +17,13 @@ public class UserController : Controller
 {
     private readonly IWorkUnit _workUnit;
     private readonly AppDataContext _context;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public UserController(IWorkUnit workUnit, AppDataContext context)
+    public UserController(IWorkUnit workUnit, AppDataContext context, UserManager<IdentityUser> userManager)
     {
         _workUnit = workUnit;
         _context = context;
+        _userManager = userManager;
     }
 
     public async Task<IActionResult> Index()
@@ -83,6 +86,66 @@ public class UserController : Controller
 
         await _workUnit.SaveAsync();
         TempData["SuccessfulOperation"] = $"User {specificUser.Name} was unsuspended";
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> ManageUser(string userId)
+    {
+        string roleId = (await _context.UserRoles.FirstOrDefaultAsync(r => r.UserId == userId))!.RoleId;
+
+        UserViewModel userViewModel = new()
+        {
+            User = await _workUnit.UserRepo.GetAsync(u => u.Id == userId, "Company"),
+            Role = ((await _context.Roles.FirstOrDefaultAsync(r => r.Id == roleId))!).Name
+        };
+
+        UserManagementVewModel managementVewModel = new()
+        {
+            UserDetails = userViewModel,
+            AvailableAppRoles = _context.Roles.Select(r => new SelectListItem
+            {
+                Text = r.Name,
+                Value = r.Name
+            }),
+            Companies = _context.Companies.Select(c => new SelectListItem
+            {
+                Text = c.Name,
+                Value = c.Id.ToString(),
+            })
+        };
+
+        return View(managementVewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RoleManagement(UserManagementVewModel viewModel)
+    {
+        string roleId =
+            ((await _context.UserRoles.FirstOrDefaultAsync(u => u.UserId == viewModel.UserDetails.User.Id))!)
+            .RoleId;
+
+        string previousRoleValue = ((await _context.Roles.FirstOrDefaultAsync(r => r.Id == roleId))!).Name!;
+
+        if (viewModel.UserDetails.Role != previousRoleValue)
+        {
+            AppUser user = await _workUnit.UserRepo.GetAsync(u => u.Id == viewModel.UserDetails.User.Id);
+
+            if (viewModel.UserDetails.Role == RoleManager.Company)
+            {
+                user.CompanyId = viewModel.UserDetails.User.CompanyId;
+            }
+
+            if (previousRoleValue == RoleManager.Company)
+            {
+                user.CompanyId = null;
+            }
+
+            await _workUnit.SaveAsync();
+
+            await _userManager.RemoveFromRoleAsync(user, previousRoleValue);
+            await _userManager.AddToRoleAsync(user, viewModel.UserDetails.Role!);
+        }
+
         return RedirectToAction(nameof(Index));
     }
 }
--- BookBazaarWeb/wwwroot/js/ToggleCompanyDropDownList.js
+++ BookBazaarWeb/wwwroot/js/ToggleCompanyDropDownList.js
@@ -1,17 +1,35 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
-    const inputRole = document.querySelector('#Input_Role');
-    const companyFormElement = document.querySelector('.CompanyWrapper');
+﻿if (window.location.href.includes(`https://localhost:7279/Admin/User/ManageUser?userId=`)) {
+    document.addEventListener("DOMContentLoaded", () => {
+        const inputRole = document.querySelector('#UserDetails_Role');
+        const companyFormElement = document.querySelector('.CompanyWrapper');
 
 
-    inputRole.addEventListener('change', () => {
-        if (inputRole.options[inputRole.selectedIndex].text === "Company") {
-            companyFormElement.classList.remove('Invisible');
-            companyFormElement.classList.add('Visible');
-        } else {
-            if (!companyFormElement.classList.contains('Invisible')) {
-                companyFormElement.classList.add('Invisible');
+        inputRole.addEventListener('change', () => {
+            if (inputRole.options[inputRole.selectedIndex].text === "Company") {
+                companyFormElement.classList.remove('Invisible');
+                companyFormElement.classList.add('Visible');
+            } else {
+                if (!companyFormElement.classList.contains('Invisible')) {
+                    companyFormElement.classList.add('Invisible');
+                }
             }
-        }
-    })
+        })
+    });
+} else {
+    document.addEventListener("DOMContentLoaded", () => {
+        const inputRole = document.querySelector('#Input_Role');
+        const companyFormElement = document.querySelector('.CompanyWrapper');
 
-});
+
+        inputRole.addEventListener('change', () => {
+            if (inputRole.options[inputRole.selectedIndex].text === "Company") {
+                companyFormElement.classList.remove('Invisible');
+                companyFormElement.classList.add('Visible');
+            } else {
+                if (!companyFormElement.classList.contains('Invisible')) {
+                    companyFormElement.classList.add('Invisible');
+                }
+            }
+        })
+    });
+}
